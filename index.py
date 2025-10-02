38b4978667b74418080a0c6dc372d13fc9894c3b
saidElamri <said.elamri.evil@gmail.com>
2025-10-02 09:56:39 +0100

    templates2

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import markdown
 import os
 
 # Fix template path for Vercel
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'templates')
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),  'templates')
 app = Flask(__name__, template_folder=template_dir)
 
 # Configure Gemini - Use environment variable for security
